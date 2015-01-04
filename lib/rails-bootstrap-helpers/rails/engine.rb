d371cce1230d86a91207eff5bb4d441b02846bfb
Henrik <hb@enovate.de>
2015-01-05 00:59:34 +0100

    tabbable only slim version

--- lib/rails-bootstrap-helpers/rails/engine.rb
+++ lib/rails-bootstrap-helpers/rails/engine.rb
@@ -2,14 +2,6 @@ module RailsBootstrapHelpers
   module Rails
     class Engine < ::Rails::Engine
       initializer "rails-bootstrap-helpers.helpers" do
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::AccordionHelper
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::UrlHelper
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::AlertHelper
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::BaseHelper
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::ButtonHelper
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::FormTagHelper
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::LabelHelper
-        ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::TagHelper
         ActionView::Base.send :include, RailsBootstrapHelpers::Helpers::NavigationHelper
       end
     end
